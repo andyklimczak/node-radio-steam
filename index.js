411478d5d91ed68ab0c8444c32d1faf952e8f9c2
Andy Klimczak <andyklimczak@fastmail.com>
2018-04-22 19:48:29 -0400

    more

--- index.js
+++ index.js
@@ -6,27 +6,59 @@ var Volume = require('pcm-volume');
 //var url = 'http://ice1.somafm.com/dronezone-256-mp3';
 var url = 'http://radio.plaza.one/mp3';
 
-audioOptions = {
-  channels: 2,
-  bitDepth: 16,
-  sampleRate: 44100,
-  mode: lame.STEREO
+function RadioStream() {
+  this.playing = false;
+  this.volume = 100;
+  this.url = null;
+  this.speaker = new Speaker(
+    {
+      channels: 2,
+      bitDepth: 16,
+      sampleRate: 44100,
+      mode: lame.STEREO
+  });
+  this.decoder = new lame.Decoder();
+  this.volume = new Volume();
+  this.andy = 'andy';
+};
+
+RadioStream.prototype.play = function() {
+  this.playing = true;
+  var _this = this;
+  request.get(_this.url).on('response', function(res) {
+    res.pipe(_this.decoder).once('format', function() {
+      _this.volume.pipe(_this.speaker);
+      _this.decoder.pipe(_this.volume);
+    });
+  });
+};
+
+RadioStream.prototype.stop = function() {
+  this.playing = false;
+  this.speaker.end();
+};
+
+RadioStream.prototype.setStreamUrl = function(url) {
+  this.url = url;
 }
 
-var speaker = new Speaker(audioOptions);
-var decoder = new lame.Decoder();
-var volume = new Volume();
+RadioStream.prototype.setVolume = function(volume) {
+  this.volume = volume < 0 ?
+    0 :
+    volume > 100 ?
+    100 :
+    volume;
+}
 
-setTimeout(function() {
-  console.log('volume set');
-  volume.setVolume(0.1);
-}, 5000);
+RadioStream.prototype.increaseVolume = function(volume) {
+  this.setVolume(this.volume + volume);
+}
 
-volume.pipe(speaker);
+RadioStream.prototype.decreaseVolume = function(volume) {
+  this.setVolume(this.volume - volume);
+}
 
-request.get(url).on('response', function(res) {
-  console.log(res);
-  res.pipe(decoder).once('format', function() {
-    decoder.pipe(volume);
-  });
-});
+// test
+var radio = new RadioStream();
+radio.setStreamUrl(url);
+radio.play();
